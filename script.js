947fd36329d50313314904ae9974c55d9e725231
Dhvani365 <dhvani365@gmail.com>
2023-09-05 18:48:11 +0530

    Develped Complete Website

---
+++ script.js
@@ -0,0 +1,13 @@
+var tablinks=document.getElementsByClassName("tab-links");
+        var tabcontents=document.getElementsByClassName("tab-contents");
+
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablink.classList.remove("active");
+            }
+            for(tabcontent of tabcontents){
+                tabcontent.classList.remove("activetab");
+            }
+            event.currentTarget.classList.add("active");
+            document.getElementById(tabname).classList.add("activetab");
+        }
